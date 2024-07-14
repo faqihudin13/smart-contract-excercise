6dbe424d0984379ec5d5b9ac88a86ff6935bb202
faqihudin13 <faqihudin13@gmail.com>
2024-07-14 21:32:25 +0700

    add reference

--- 6-signing-transaction.py
+++ 6-signing-transaction.py
@@ -7,12 +7,12 @@ from web3 import Web3
 #Make Connection To Local Blockchain
 GANACHE = 'http://127.0.0.1:7545'
 web3 = Web3(Web3.HTTPProvider(GANACHE))
-print(web3.isConnected())
+print(web3.is_connected())
 
 #Accounts for transactions
-first_account = "ADD Address"
-first_account_private_key = "ADD PrivateKey"
-second_account = "ADD Address"
+first_account = "0x7A1E86F65f90298641011D121de2E5c95D5FdC94"
+first_account_private_key = "0x150132990404f6e66ed5cbbd3b61fd1aa14228ff1e06685bb76777628ce9f9e1"
+second_account = "0xFb3E3D3AF7B780cD229C39bc0fC268AAa44De9BF"
 
 #Build Transaction object
 transaction = {
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 I learn from him https://www.youtube.com/@ConsoleCowboys,
 please subscribe and support
+
+and also https://github.com/cclabsInc/Python-SmartContact-BlockchainExploitation
